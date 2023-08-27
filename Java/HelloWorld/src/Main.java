e802c18805d444925ba0b8651fe617aa3c6558cf
sreak <shakilmith2@gmail.com>
2023-08-27 14:32:05 +0600

    first commit

---
+++ Java/HelloWorld/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * Author : Shakil Ahmed
+ * GitHub: @shakilmith
+ * It is a simple Hello World console application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
